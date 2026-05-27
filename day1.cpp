66bec481a0d1121a602354032895c2378e310849
kshitijmax21 <kshitijpathak2105@gmail.com>
2026-05-27 23:45:07 +0530

    updated day 1

---
+++ day1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the number you want to check";
+    cin>>n;
+    if (n%2==0)
+     cout<<"It is an Even Number";
+    else 
+     cout<<"It is An Odd Number";
+
+return 0;
+
+
+}
+
