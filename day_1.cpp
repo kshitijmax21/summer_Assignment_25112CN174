287be97c4f785e7257e8faa37e6a1b7beac66f1f
kshitijmax21 <kshitijpathak2105@gmail.com>
2026-05-28 12:14:00 +0530

    second question updated

--- day_1.cpp
+++ day_1.cpp
@@ -2,19 +2,36 @@
 
 
 
-#include<iostream>
+/* #include<iostream>
 using namespace std;
 int main(){
-    int n;
-    cout<<"enter the number you want to check";
+    int n,i,s;
+    s=0;
+    cout<<"Enter the value of N ";
     cin>>n;
-    if (n%2==0)
-     cout<<"It is an Even Number";
-    else 
-     cout<<"It is An Odd Number";
-
+    for(i=1;i<=n;i++){
+        s=s+i;
+        cout<<s;
+    }
 return 0;
 
 
-}
+} */
 
+
+
+ //Write a program to Print multiplication table of a given number
+ #include<iostream>
+ using namespace std;
+ int main (){
+    int i ,n,x;
+    cout<<"enter the number ";
+    cin>>x;
+    n=x*10;
+    for (i=x;i<=n;i=i+x){
+        cout<<i<<endl;
+    }
+     
+
+ }
+ 
