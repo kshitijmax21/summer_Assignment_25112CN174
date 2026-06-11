763fb02106da00b8494849be4067d13cde67ff9b
kshitijmax21 <kshitijpathak2105@gmail.com>
2026-06-11 21:35:54 +0530

    day 11

---
+++ day_11/sumthroughfunctions.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int sum(int a, int b){
+    return a+b;
+     
+}
+
+int main() {
+    int a,b;
+    
+    cout<<"Enter the first number ";
+    cin>> a ;
+    cout<<"Enter the Second  number ";
+    cin>> b ;
+    cout<< sum (a,b);
+}
