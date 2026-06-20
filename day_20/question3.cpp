6190805b443154f08f239511c4ee26589ce60c3a
kshitijmax21 <kshitijpathak2105@gmail.com>
2026-06-20 21:07:00 +0530

    day 20

---
+++ day_20/question3.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int rows, cols;
+    cout << "Enter number of rows and columns: ";
+    cin >> rows >> cols;
+    int a[10][10];
+    cout << "Enter the matrix elements:\n";
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            cin >> a[i][j];
+        }
+    }
+   cout << "\nRow-wise sums are:\n";
+   for(int i = 0; i < rows; i++) {
+        int sum = 0;
+        for(int j = 0; j < cols; j++) {
+            sum = sum + a[i][j];
+        }
+         cout << "Sum of row " << i + 1 << " = " << sum << endl;
+    }
+
+    return 0;
+}
---
+++ day_20/question4.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int rows, cols;
+
+    cout << "Enter number of rows and columns:";
+    cin >> rows >> cols;
+
+    int a[10][10];
+
+    cout << "Enter the matrix elements:\n";
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    cout << "\nColumn-wise sums are:\n";
+
+    for(int j = 0; j < cols; j++) {
+        int sum = 0;
+
+        for(int i = 0; i < rows; i++) {
+            sum = sum + a[i][j];
+        }
+        cout << "Sum of column " << j + 1 << " = " << sum << endl;
+    }
+
+    return 0;
+}
